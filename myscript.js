fd6b90e24760c2e574b6b695fd99e14ee6c5e01f
Iain Haslam <ijc.haslam@gmail.com>
2021-05-03 14:47:51 +0100

    added player moves and one round game

--- myscript.js
+++ myscript.js
@@ -1,5 +1,27 @@
-let moves = ["Rock", "Paper", "Scissors"]
+let moves = ["rock", "paper", "scissors"]
 function computerPlay() {
     return moves[Math.floor(Math.random() * 3)];
 }
-console.log(computerPlay())
+    let computerSelection = computerPlay;
+
+
+let playerInput = prompt("What is your move?");
+let playerSelection = playerInput.toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return("You win! Rock beats scissors.")
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+            return("You win! Paper beats rock.")
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            return("You win! Scissors beat paper.")
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            return("You lose! Rock beats scissors")
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            return("You lose! Paper beats rock")
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            return("You lose! Scissors beat paper")
+        }
+        else return("Draw!")
+    }
+console.log(playRound(playerSelection, computerSelection))
