349157cd6952b1d58c7954d6bcbbc68da09753c6
Iain Haslam <ijc.haslam@gmail.com>
2021-05-04 17:10:14 +0100

    Big changes: cleaned up the code and added comments for each stage. Worked out how to tidy everything up and keep everything scoped correctly

--- myscript.js
+++ myscript.js
@@ -1,38 +1,76 @@
+//Initial scores
+let playerScore = 0;
+let computerScore = 0;
 
-   let playerScore = 0
-   let computerScore = 0
-   let moves = ["rock", "paper", "scissors"]
-    function computerPlay() {
-        return moves[Math.floor(Math.random() * 3)];
+// Producing computer moves. 
+function computerPlay() {
+    let computerMoves = ['rock', 'paper', 'scissors'];
+    let computerChoices = Math.floor(Math.random() * 3);
+    return computerMoves[computerChoices];
+}
+
+// 2... Make a function that plays a single round of Rock, Paper, Scissors.
+function playRound(playerSelection, computerSelection) {
+    console.log(`You selected: "${playerSelection}"`);
+    console.log(`Computer selected: "${computerSelection}"`);
+
+//Draw
+    if (playerSelection === computerSelection) {
+        console.log('Draw!')
+        return;
     }
-    let computerSelection = computerPlay();
-
-
-    let playerInput = prompt("What is your move?");
-    let playerSelection = playerInput.toLowerCase();
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && computerSelection === "scissors") {
-            ++playerScore
-            return("You win! Rock beats scissors.")
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            ++playerScore
-            return("You win! Paper beats rock.");
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            ++playerScore
-            return("You win! Scissors beat paper.")
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            ++computerScore
-            return("You lose! Rock beats scissors")
-        } else if (playerSelection === "rock" && computerSelection === "paper") {
-            ++computerScore
-            return("You lose! Paper beats rock")
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            ++computerScore
-            return("You lose! Scissors beat paper")
+//Player chooses Rock
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            playerScore++
+            console.log('You win! Rock beats scissors.')
+            return; 
+        } else {
+            computerScore++
+            console.log('You lose! Paper beats Rock!')
+            return;
         }
-        else return("Draw!")
     }
-console.log(playRound(playerSelection, computerSelection))
-console.log(playerScore)
-console.log(computerScore)
+
+//Player chooses Paper
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            playerScore++
+            console.log('You win! Paper beats Rock.')
+            return;
+        } else {
+            computerScore++
+            console.log('You lose! Scissors beat paper.')
+            return;
+        }
+    }
+
+//Player chooses Scissors
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            playerScore++
+            console.log('You win! Scissors beat Paper.')
+            return;
+        } else {
+            computerScore++
+            console.log('You lose! Rock beats scissors.')
+            return;
+        }
+    }
+}
+
+// Play a game using prompt and computerPlay function
+function game() {
+    const playerInput = prompt('What is your move?')
+    playRound(playerInput.toLowerCase(), computerPlay())
+    console.log(playerScore)
+    console.log(computerScore)
+    return;
+}
+
+// Play 5 games in a row. 
+game()
+game()
+game()
+game()
+game()
